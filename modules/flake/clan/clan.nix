4618f58bef14a0dfc0a4e61d25dbb853a0669134
clan-tool <clan@example.com>
2025-09-02 16:55:58 -0600

    Ran flake check
    Formatting

--- modules/flake/clan/clan.nix
+++ modules/flake/clan/clan.nix
@@ -2,7 +2,7 @@
   clan = {
     # Ensure this is unique among all clans you want to use.
     meta.name = "Dreamweave";
-  
+
     inventory.machines = {
       # Define machines here.
       "Trance" = {
@@ -10,10 +10,10 @@
         tags = [ ];
       };
     };
-  
+
     # Docs: See https://docs.clan.lol/reference/clanServices
     inventory.instances = {
-  
+
       # Docs: https://docs.clan.lol/reference/clanServices/admin/
       # Admin service for managing machines
       # This service adds a root password and SSH access.
@@ -23,10 +23,11 @@
           # Insert the public key that you want to use for SSH access.
           # All keys will have ssh access to all machines ("tags.all" means 'all machines').
           # Alternatively set 'users.users.root.openssh.authorizedKeys.keys' in each machine
-          "admin-machine-1" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+LhElRovxT0LgPhodvbh0TIsEDlRPrAF7JxvcjH0s1 brettk@nixos";
+          "admin-machine-1" =
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+LhElRovxT0LgPhodvbh0TIsEDlRPrAF7JxvcjH0s1 brettk@nixos";
         };
       };
-  
+
       # Docs: https://docs.clan.lol/reference/clanServices/zerotier/
       # The lines below will define a zerotier network and add all machines as 'peer' to it.
       # !!! Manual steps required:
@@ -41,22 +42,22 @@
         # tags.all means 'all machines' will joined
         roles.peer.tags.all = { };
       };
-  
+
       # Docs: https://docs.clan.lol/reference/clanServices/tor/
       # Tor network provides secure, anonymous connections to your machines
       # All machines will be accessible via Tor as a fallback connection method
       tor = {
         roles.server.tags.nixos = { };
       };
-  
-      jon-user = { # 
-  
+
+      jon-user = {
+
         module.name = "users";
-  
-        roles.default.tags.all = { }; # 
-  
+
+        roles.default.tags.all = { };
+
         roles.default.settings = {
-          user = "Brett"; # 
+          user = "Brett";
           groups = [
             "wheel" # Allow using 'sudo'
             "networkmanager" # Allows to manage network connections.
@@ -66,17 +67,19 @@
         };
       };
     };
-  
+
     # Additional NixOS configuration can be added here.
     # machines/jon/configuration.nix will be automatically imported.
     # See: https://docs.clan.lol/guides/more-machines/#automatic-registration
     machines = {
-      "Trance" = { config, pkgs, ... }: {
-        environment.systemPackages = [ pkgs.asciinema ];
-        users.users.root.openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+LhElRovxT0LgPhodvbh0TIsEDlRPrAF7JxvcjH0s1 brettk@nixos"
-        ];
-      };
+      "Trance" =
+        { pkgs, ... }:
+        {
+          environment.systemPackages = [ pkgs.asciinema ];
+          users.users.root.openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+LhElRovxT0LgPhodvbh0TIsEDlRPrAF7JxvcjH0s1 brettk@nixos"
+          ];
+        };
     };
   };
 }
--- modules/flake/formatter.nix
+++ modules/flake/formatter.nix
@@ -1,8 +1,17 @@
 {
-  perSystem.treefmt.projectRootFile = "flake.nix";
-  perSystem.treefmt.programs = {
-    nixfmt.enable = true;
-    nixfmt.excludes = [ ".direnv" "sops/*" "vars/*" "machines/*" ];
-    deadnix.enable = true;
+  perSystem.treefmt = {
+    projectRootFile = "flake.nix";
+    programs = {
+      nixfmt.enable = true;
+      nixfmt.excludes = [ ".direnv" ];
+      deadnix.enable = true;
+    };
+    settings = {
+      global.excludes = [
+        "vars/**"
+        "sops/**"
+        "machines/**"
+      ];
+    };
   };
 }
