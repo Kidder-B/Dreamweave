14ee4ac0b71165c8028b79afd61d3ad63a6196c6
clan-tool <clan@example.com>
2025-09-03 14:25:11 -0600

    Added brett.nix
    Formatting
    
    Removed homeManager reference
    
    Removed unnecessary input
    
    Removed unnecessary input statement

---
+++ modules/users/brett.nix
@@ -0,0 +1,25 @@
+let
+  clan.inventory.instances = {
+    brett-user = {
+
+      module.name = "users";
+
+      roles.default.tags.all = { };
+
+      roles.default.settings = {
+        user = "Brett";
+        groups = [
+          "wheel" # Allow using 'sudo'
+          "networkmanager" # Allows to manage network connections.
+          "video" # Allows to access video devices.
+          "input" # Allows to access input devices.
+        ];
+      };
+      roles.default.extraModules = [
+      ];
+    };
+  };
+in
+{
+  inherit clan;
+}
