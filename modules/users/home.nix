e7009489593501af690ddac054ff15e6a933e1df
clan-tool <clan@example.com>
2025-09-03 15:22:53 -0600

    Added firefox to user brett

--- modules/users/home.nix
+++ modules/users/home.nix
@@ -1,12 +1,16 @@
 { inputs, ... }:
 let
   flake.modules.nixos."Brett" = {
+    imports = [
+      inputs.home-manager.nixosModules.home-manager
+    ];
+
     home-manager = {
       useGlobalPkgs = true;
       useUserPackages = true;
 
       users."Brett".imports = with inputs.self.modules.homeManager; [
-        home-manager
+        firefox
       ];
     };
   };
