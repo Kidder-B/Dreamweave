323863b7fd89d1cae66a25930863d15fd2a3fecf
clan-tool <clan@example.com>
2025-09-02 23:43:53 -0600

    Updated gnome-desktop.nix

--- modules/nixos/gnome-desktop.nix
+++ modules/nixos/gnome-desktop.nix
@@ -1,7 +1,7 @@
 {
   flake.modules.nixos.gnome-desktop = {
     # Enable the GNOME Desktop Environment.
-    services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = true;
+    services.displayManager.gdm.enable = true;
+    services.desktopManager.gnome.enable = true;
   };
 }
