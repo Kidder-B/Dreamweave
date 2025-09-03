3fc87696221c1c810038336f05f1a0c552432d23
clan-tool <clan@example.com>
2025-09-03 14:25:03 -0600

    Tweaked Trance.nix for easier list merging in future
    Formatting

--- modules/machines/trance.nix
+++ modules/machines/trance.nix
@@ -1,11 +1,14 @@
 { inputs, ... }:
 let
-  clan.machines."Trance".imports = with inputs.self.modules.nixos; [
-    gnome-desktop
-    unfree
-    nvidia
-    all-firmware
-  ];
+  clan.machines."Trance".imports = (
+    with inputs.self.modules.nixos;
+    [
+      gnome-desktop
+      unfree
+      nvidia
+      all-firmware
+    ]
+  );
 in
 {
   inherit clan;
