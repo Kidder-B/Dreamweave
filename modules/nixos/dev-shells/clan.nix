4dbdbb9a065d54fd0f3e9b7a280ff61cd9a1de37
clan-tool <clan@example.com>
2025-09-04 16:29:50 -0600

    Added clan.nix to devshells dir

---
+++ modules/nixos/dev-shells/clan.nix
@@ -0,0 +1,16 @@
+{ inputs, ... }
+{
+  devShells =
+    nixpkgs.lib.genAttrs
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ]
+      (system: {
+        default = inputs.clan-core.inputs.nixpkgs.legacyPackages.${system}.mkShell {
+          packages = [ inputs.clan-core.packages.${system}.clan-cli ];
+        };
+      });
+}
