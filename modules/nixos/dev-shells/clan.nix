acbb6675dd8a918668351f864511deaaf6f4c867
clan-tool <clan@example.com>
2025-09-04 23:01:38 -0600

    Adjusted clan devshell to use flake-parts
    Formatting

--- modules/nixos/dev-shells/clan.nix
+++ modules/nixos/dev-shells/clan.nix
@@ -1,16 +1,8 @@
 { inputs, ... }:
 {
-  perSystem.devshells =
-    inputs.nixpkgs.lib.genAttrs
-      [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ]
-      (system: {
-        default = inputs.clan-core.inputs.nixpkgs.legacyPackages.${system}.mkShell {
-          packages = [ inputs.clan-core.packages.${system}.clan-cli ];
-        };
-      });
+  perSystem =
+    { system, ... }:
+    {
+      devshells.default.packages = [ inputs.clan-core.packages.${system}.clan-cli ];
+    };
 }
