6a08c6cbb7d5abcff501711395382e5d50f720c0
clan-tool <clan@example.com>
2025-09-16 15:19:06 -0600

    Reformatted dreamweave.nix

--- modules/home-profiles/brett/dreamweave.nix
+++ modules/home-profiles/brett/dreamweave.nix
@@ -1,23 +1,23 @@
 let
   flake.modules.homeManager."Brett" =
-  { lib, ... }:
-  {
-home.activation.updateGitRepo = lib.hm.dag.entryAfter ["writeBoundary"] ''
-  REPO_DIR="$HOME/Clan/Dreamweave"
-  GIT_URL="https://github.com/yourusername/my-repo.git"
+    { lib, ... }:
+    {
+      home.activation.updateGitRepo = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        REPO_DIR="$HOME/Clan/Dreamweave"
+        GIT_URL="https://github.com/yourusername/my-repo.git"
 
-  mkdir -p "$HOME/Clan"
+        mkdir -p "$HOME/Clan"
 
-  if [ -d "$REPO_DIR/.git" ]; then
-    echo "Updating existing git repo at $REPO_DIR"
-    git -C "$REPO_DIR" pull --rebase
-  else
-    echo "Cloning git repo into $REPO_DIR"
-    git clone "$GIT_URL" "$REPO_DIR"
-  fi
-'';
-};
+        if [ -d "$REPO_DIR/.git" ]; then
+          echo "Updating existing git repo at $REPO_DIR"
+          git -C "$REPO_DIR" pull --rebase
+        else
+          echo "Cloning git repo into $REPO_DIR"
+          git clone "$GIT_URL" "$REPO_DIR"
+        fi
+      '';
+    };
 in
 {
-  inherit flake
+  inherit flake;
 }
