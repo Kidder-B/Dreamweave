53813e57eebfd1d6ace2c1fabc828baec0dfa0d8
clan-tool <clan@example.com>
2025-09-15 22:36:30 -0600

    Moved to fragment system
    Not pretty, but not wholly inelegant
    
    Typo

--- modules/clan/services/unfree.nix
+++ modules/clan/services/unfree.nix
@@ -1,12 +1,9 @@
 { inputs, ... }:
 let
-  inherit (inputs.self.lib.clan)
-    mkService
-    mkTag
-    ;
-
-  clan.modules = mkService "unfree" (with inputs.self.modules.nixos; [ unfree ]);
-  clan.inventory.instances = mkTag "unfree";
+  inherit (inputs.self.lib.clan) mkTaggedService;
+  fragment = mkTaggedService "unfree" (with inputs.self.modules.nixos; [ unfree ]);
+  clan.modules = fragment.modules;
+  clan.inventory.instances = fragment.instances;
 in
 {
   inherit clan;
