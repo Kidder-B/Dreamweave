f25bcfa4a4d9f86a8f40c00bcf1650c3bad3b5dd
clan-tool <clan@example.com>
2025-09-16 13:51:01 -0600

    Added Dreamweave.nix
    For managing this clan on all machines with Brett

---
+++ modules/home-profiles/brett/dreamweave.nix
@@ -0,0 +1,18 @@
+{
+  flake.modules.homeManager."Brett" = {
+home.activation.updateGitRepo = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  REPO_DIR="$HOME/Clan/Dreamweave"
+  GIT_URL="https://github.com/yourusername/my-repo.git"
+
+  mkdir -p "$HOME/Clan"
+
+  if [ -d "$REPO_DIR/.git" ]; then
+    echo "Updating existing git repo at $REPO_DIR"
+    git -C "$REPO_DIR" pull --rebase
+  else
+    echo "Cloning git repo into $REPO_DIR"
+    git clone "$GIT_URL" "$REPO_DIR"
+  fi
+'';
+};
+}
