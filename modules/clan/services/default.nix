80336ed500db790a1dbd469613f1eca4f1347d6d
clan-tool <clan@example.com>
2025-09-17 17:17:17 -0600

    Added shared-vars module to default imports

--- modules/clan/services/default.nix
+++ modules/clan/services/default.nix
@@ -1,17 +1,23 @@
 { inputs, ... }:
 let
   inherit (inputs.self.lib.clan) mkTaggedService;
+
   fragment = mkTaggedService "all" (
     with inputs.self.modules.nixos;
-    [
-      all-firmware
-      git
-      neovim
-    ]
+    (
+      [
+        all-firmware
+        git
+        neovim
+        shared-vars
+      ]
+    )
   );
+
   clan.modules = fragment.modules;
   clan.inventory.instances = fragment.instances;
 in
 {
   inherit clan;
 }
+
