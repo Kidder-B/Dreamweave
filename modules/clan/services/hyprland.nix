7a18dc56f719a0729c448da7f1403399a1b003f0
clan-tool <clan@example.com>
2025-09-18 11:31:42 -0600

    Updated hyprland service to accomodate nvidia

--- modules/clan/services/hyprland.nix
+++ modules/clan/services/hyprland.nix
@@ -2,8 +2,28 @@
 let
   inherit (inputs.self.lib.clan) mkTaggedService;
   fragment = mkTaggedService "hyprland" (with inputs.self.modules.nixos; [ hyprland ]);
-  clan.modules = fragment.modules;
-  clan.inventory.instances = fragment.instances;
+
+  clan = {
+    modules = fragment.modules // {
+      hyprland = (fragment.modules."hyprland" or { }) // {
+        roles.nvidia = { };
+        roles.nvidia.perInstance = {
+          nixosModule = {
+            boot.kernelParams = [
+              "nvidia-drm.modeset=1"
+              "nouveau.modeset=0"
+            ];
+          };
+        };
+      };
+    };
+
+    inventory.instances = fragment.instances // {
+      "hyprland" = (fragment.instances."hyprland" or { }) // {
+        roles.nvidia.tags."nvidia" = { };
+      };
+    };
+  };
 in
 {
   inherit clan;
