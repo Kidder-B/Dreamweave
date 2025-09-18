465550288d7e098f9aedf324fd38080f686a93d5
clan-tool <clan@example.com>
2025-09-18 10:30:37 -0600

    Removed redundant inputs

--- modules/imports/hyprland-plugins.nix
+++ modules/imports/hyprland-plugins.nix
@@ -1,11 +1,9 @@
-{ inputs, ... }:
 {
   flake-file.inputs = {
     hyprland.url = "github:hyprwm/Hyprland";
     hy3 = {
-        url = "github:outfoxxed/hy3";
-        inputs.hyprland.follows = "hyprland";
+      url = "github:outfoxxed/hy3";
+      inputs.hyprland.follows = "hyprland";
     };
   };
 }
-
