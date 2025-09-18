a2d203d0314fe3d724fa8f2ea74a6a46ccd24279
clan-tool <clan@example.com>
2025-09-17 21:12:42 -0600

    Removed github.nix
    Moved functionality into sshd.nix

--- modules/home-profiles/brett/github.nix
+++
@@ -1,8 +0,0 @@
-{
-  flake.modules.homeManager."Brett" =
-    { osConfig, ... }:
-    {
-      programs.ssh.userKnownHostsFile =
-        osConfig.clan.core.vars.generators."github-known-hosts".files."known_hosts".path;
-    };
-}
