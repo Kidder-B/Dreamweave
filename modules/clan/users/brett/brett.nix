104cceae62d6d154fa72b7151af45edcb5bab191
clan-tool <clan@example.com>
2025-09-17 20:56:26 -0600

    Moved user brett to its own directory.

---
+++ modules/clan/users/brett/brett.nix
@@ -0,0 +1,51 @@
+{ inputs, lib, ... }:
+
+let
+  name = "Brett";
+  lowerName = lib.strings.toLower name;
+
+  clan = {
+    modules.${name} = {
+      _class = "clan.service";
+      manifest.name = name;
+
+      roles.default = { };
+
+      perMachine.nixosModule = {
+        # Define system user with extra groups
+        users.users.${name} = {
+
+          initialPassword = "password";
+          isNormalUser = true;
+          extraGroups = [
+            "wheel"
+            "networkmanager"
+            "video"
+            "input"
+          ];
+        };
+
+        # Import Home Manager and configure user
+        imports = [ inputs.home-manager.nixosModules.home-manager ];
+
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+
+          users.${name} = {
+            imports = [ inputs.self.modules.homeManager.${name} ];
+          };
+        };
+      };
+    };
+
+    inventory.instances."${lowerName}-user" = {
+      module.input = "self";
+      module.name = name;
+      roles.default.tags.${lowerName} = { };
+    };
+  };
+in
+{
+  inherit clan;
+}
