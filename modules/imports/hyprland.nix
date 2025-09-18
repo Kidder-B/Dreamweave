511f13ed868137aab31c8aa931d076f7d36e26ac
clan-tool <clan@example.com>
2025-09-18 10:29:17 -0600

    Added hyprland imports

---
+++ modules/imports/hyprland.nix
@@ -0,0 +1,11 @@
+{ inputs, ... }:
+{
+  flake-file.inputs = {
+    hyprland.url = "github:hyprwm/Hyprland";
+    hy3 = {
+        url = "github:outfoxxed/hy3";
+        inputs.hyprland.follows = "hyprland";
+    };
+  };
+}
+
