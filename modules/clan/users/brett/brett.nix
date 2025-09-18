df95f7fd847f921bec4303bcd8f3770f2988b305
clan-tool <clan@example.com>
2025-09-18 08:15:07 -0600

    Adjusted user to use password generator

--- modules/clan/users/brett/brett.nix
+++ modules/clan/users/brett/brett.nix
@@ -7,39 +7,61 @@ let
   clan = {
     modules.${name} = {
       _class = "clan.service";
-      manifest.name = name;
+      manifest.name = "@dreamweave/brett-user";
+      manifest.description = ''
+        Create the "Brett" system user, its group, initial password and
+        Home Manager imports. This is a bespoke service for the Brett user.
+      '';
 
-      roles.default = { };
+      roles.default = {
+        perInstance = {
+          nixosModule =
+            let
+              userName = "Brett";
+            in
+            {
+              # Ensure group exists
+              users.groups.${userName} = { };
 
-      perMachine.nixosModule = {
-        # Define system user with extra groups
-        users.groups.${name} = { };
-        users.users.${name} = {
+              # System user definition using generated hashed password file
+              users.users.${userName} = {
+                isNormalUser = true;
+                extraGroups = [
+                  userName
+                  "wheel"
+                  "networkmanager"
+                  "video"
+                  "input"
+                ];
 
-          initialPassword = "password";
-          isNormalUser = true;
-          extraGroups = [
-            name
-            "wheel"
-            "networkmanager"
-            "video"
-            "input"
-          ];
+                #hashedPasswordFile =
+                #  config.clan.core.vars.generators."user-password-${userName}".files.user-password-hash.path;
+              };
+
+              # Import Home Manager and per-user home-manager module
+              imports = [
+                inputs.home-manager.nixosModules.home-manager
+                inputs.self.modules.nixos.${userName}
+                inputs.self.modules.nixos.user-password
+              ];
+
+              # Home Manager configuration for the user
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+
+                users.${userName} = {
+                  imports = [ inputs.self.modules.homeManager.${userName} ];
+                };
+              };
+            };
         };
+      };
 
-        # Import Home Manager and configure user
-        imports = [
-          inputs.home-manager.nixosModules.home-manager
-          inputs.self.modules.nixos.${name}
-        ];
-
-        home-manager = {
-          useGlobalPkgs = true;
-          useUserPackages = true;
-
-          users.${name} = {
-            imports = [ inputs.self.modules.homeManager.${name} ];
-          };
+      perMachine = {
+        nixosModule = {
+          # Immutable users so this module controls users exclusively.
+          users.mutableUsers = false;
         };
       };
     };
