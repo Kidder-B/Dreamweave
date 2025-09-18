530dd4a52423d6ce771c472d4804b04acac4e6c8
clan-tool <clan@example.com>
2025-09-17 21:22:49 -0600

    Adjusted ssh-key generation for privacy

--- modules/clan/users/brett/ssh-key.nix
+++ modules/clan/users/brett/ssh-key.nix
@@ -1,21 +1,25 @@
-{ config, pkgs, ... }: {
-  clan.core.vars.generators.brett-ssh-key = {
-    share = true; # Omit if you want unique keys per machine
-    files."id_ed25519" = {
-      secret = true;
-      owner = "Brett";
-      group = "Brett";
-      mode = "0600";
+{
+  flake.modules.nixos."Brett" =
+    { pkgs, ... }:
+    {
+      clan.core.vars.generators.brett-ssh-key = {
+        share = true; # Omit if you want unique keys per machine
+        files."id_ed25519" = {
+          secret = true;
+          owner = "Brett";
+          group = "Brett";
+          mode = "0400";
+        };
+        files."id_ed25519.pub" = {
+          secret = true;
+          owner = "Brett";
+          group = "Brett";
+          mode = "0400";
+        };
+        runtimeInputs = [ pkgs.openssh ];
+        script = ''
+          ssh-keygen -t ed25519 -N "" -f $out/id_ed25519
+        '';
+      };
     };
-    files."id_ed25519.pub" = {
-      secret = false;
-      owner = "Brett";
-      group = "Brett";
-      mode = "0644";
-    };
-    runtimeInputs = [ pkgs.openssh ];
-    script = ''
-      ssh-keygen -t ed25519 -N "" -f $out/id_ed25519
-    '';
-  };
 }
