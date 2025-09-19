298320ca4f2d9d493b104bb075c739fab34d95a8
Kidder-B <kidderbrett@gmail.com>
2025-09-19 21:15:18 +0000

    Formatting

--- modules/home-profiles/brett/nixvim/enable.nix
+++ modules/home-profiles/brett/nixvim/enable.nix
@@ -1,10 +1,11 @@
-{inputs, ...}:
+{ inputs, ... }:
 {
-  flake.modules.homeManager."Brett" = {
-          home.packages = [
-            # Option A: Use default configuration
-            inputs.khanelivim.packages.x86_64-linux.default
-	    ];
-	    };
-	    }
-
+  flake.modules.homeManager."Brett" =
+    { pkgs, ... }:
+    {
+      home.packages = [
+        # Option A: Use default configuration
+        inputs.khanelivim.packages.${pkgs.system}.default
+      ];
+    };
+}
