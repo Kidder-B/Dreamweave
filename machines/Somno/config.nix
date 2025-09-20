cc6cbea7aa188844073f329384afd5dcb5e48a2b
Kidder-B <kidderbrett@gmail.com>
2025-09-20 21:06:27 +0000

    Tweaks to allow for remote booting
    Further tweaks
    
    More tweaking

--- machines/Somno/config.nix
+++ machines/Somno/config.nix
@@ -1,30 +1,38 @@
 {
-  boot.initrd.systemd = {
-    enable = true;
-  };
+  users.root.shell = "/bin/systemd-tty-ask-password-agent";
 
-  # uncomment this if you want to be asked for the decryption password on login
-  #users.root.shell = "/bin/systemd-tty-ask-password-agent";
+  boot = {
+    kernelModules = [ "r8169" ];
+    kernelParams = [ "loglevel=7" ];
 
-  boot.initrd.network = {
-    enable = true;
-
-    ssh = {
-      enable = true;
-      port = 7172;
-      authorizedKeys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfOhubXnAeYNgXq+XQG87RD/rvT8p0euZdTbxB0X+gd nixbld@nixos"
+    initrd = {
+      kernelModules = [ "r8169" ];
+      availableKernelModules = [
+        "xhci_pci"
       ];
-      hostKeys = [
-        "/var/lib/initrd_host_ed25519_key"
-        "/var/lib/initrd_host_rsa_key"
-      ];
+      systemd.enable = true;
+
+      network = {
+        enable = true;
+
+        ssh = {
+          enable = true;
+          port = 7171;
+          authorizedKeys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfOhubXnAeYNgXq+XQG87RD/rvT8p0euZdTbxB0X+gd nixbld@nixos"
+          ];
+          hostKeys = [
+            "/var/lib/initrd_host_ed25519_key"
+            "/var/lib/initrd_host_rsa_key"
+          ];
+        };
+      };
     };
   };
-  boot.initrd.availableKernelModules = [
-    "xhci_pci"
-  ];
 
-  # Find out the required network card driver by running `lspci -k` on the target machine
-  boot.initrd.kernelModules = [ "r8169" ];
+  # Networking setup post-decryption
+  networking = {
+    useDHCP = true; # Enable DHCP for network configuration during boot
+    networkmanager.enable = false; # Disable NetworkManager to avoid conflicts with initrd network setup
+  };
 }
