ab23350a350fef873ff638dc84d17b86cc1469b3
Kidder-B <kidderbrett@gmail.com>
2025-09-20 03:34:39 +0000

    Included full dashboard

--- modules/home-profiles/brett/nixvim/dashboard.nix
+++ modules/home-profiles/brett/nixvim/dashboard.nix
@@ -1,11 +1,43 @@
 {
   flake.modules.nixvim."Brett" =
-    { lib, ... }:
+    { config, lib, ... }:
     {
-      plugins.mini-starter = {
-        settings = {
-          content_hooks = {
-            "__unkeyed-1.adding_bullet".__raw = lib.mkForce ''require('mini.starter').gen_hook.bullet(" ")'';
+      plugins = {
+        mini-starter = {
+          enable = true;
+          settings = {
+            header = ''
+              ██╗  ██╗██╗  ██╗ █████╗ ███╗   ██║███████╗██║     ██║██╗   ██║██║████╗ ████║
+              ██║ ██╔╝██║  ██║██╔══██╗████╗  ██║██╔════╝██║     ██║██║   ██║██║████╗ ████║
+              █████╔╝ ███████║███████║██╔██╗ ██║█████╗  ██║     ██║██║   ██║██║██╔████╔██║
+              ██╔═██╗ ██╔══██║██╔══██║██║╚██╗██║██╔══╝  ██║     ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+              ██║  ██╗██║  ██║██║  ██║██║ ╚████║███████╗███████╗██║ ╚████╔╝ ██║██║ ╚═╝ ██║
+              ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚══════╝╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+            '';
+
+            evaluate_single = true;
+
+            items = {
+              "__unkeyed-1.builtin_actions".__raw = "require('mini.starter').sections.builtin_actions()";
+              "__unkeyed-2.recent_files_current_directory".__raw =
+                "require('mini.starter').sections.recent_files(10, true)";
+              "__unkeyed-3.recent_files".__raw = "require('mini.starter').sections.recent_files(10, false)";
+              "__unkeyed-4.sessions".__raw = lib.mkIf (
+                config.plugins.mini.enable && lib.hasAttr "sessions" config.plugins.mini.modules
+              ) "require('mini.starter').sections.sessions(5, true)";
+              __unkeyed-5 = lib.mkIf config.plugins.persistence.enable {
+                name = "Restore session";
+                action.__raw = "[[lua require('persistence').load()]]";
+                section = "Session";
+              };
+            };
+
+            content_hooks = {
+              "__unkeyed-1.adding_bullet".__raw = "require('mini.starter').gen_hook.adding_bullet()";
+              "__unkeyed-2.indexing".__raw =
+                "require('mini.starter').gen_hook.indexing('all', { 'Builtin actions' })";
+              "__unkeyed-3.padding".__raw = "require('mini.starter').gen_hook.aligning('center', 'center')";
+            };
           };
         };
       };
