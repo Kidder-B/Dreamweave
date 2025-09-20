775da5f142e10f4bb358e9b6640851aa943a5060
Kidder-B <kidderbrett@gmail.com>
2025-09-20 02:31:02 +0000

    Updated dashboard

--- modules/home-profiles/brett/nixvim/dashboard.nix
+++ modules/home-profiles/brett/nixvim/dashboard.nix
@@ -1,11 +1,13 @@
-{ lib, ... }:
-
 {
-  flake.modules.nixvim."Brett" = {
-    plugins = {
-      mini-starter = {
-        enable = lib.mkForce false;
+  flake.modules.nixvim."Brett" =
+    { lib, ... }:
+    {
+      plugins.mini-starter = {
+        settings = {
+          content_hooks = {
+            "__unkeyed-1.adding_bullet".__raw = lib.mkForce ''require('mini.starter').gen_hook.bullet(" ")'';
+          };
+        };
       };
     };
-  };
 }
