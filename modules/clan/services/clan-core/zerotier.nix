a961dacf41abdacf5b751271b67cfd15890de97a
Kidder-B <kidderbrett@gmail.com>
2025-09-21 22:10:55 +0000

    Creates zerotier.nix service module

---
+++ modules/clan/services/clan-core/zerotier.nix
@@ -0,0 +1,19 @@
+{
+  clan.inventory.instances = {
+    # Docs: https://docs.clan.lol/reference/clanServices/zerotier/
+    # The lines below will define a zerotier network and add all machines as 'peer' to it.
+    # !!! Manual steps required:
+    #   - Define a controller machine for the zerotier network.
+    #   - Deploy the controller machine first to initilize the network.
+    zerotier = {
+      # Replace with the name (string) of your machine that you will use as zerotier-controller
+      # See: https://docs.zerotier.com/controller/
+      # Deploy this machine first to create the network secrets
+      roles.controller.machines."Trance" = { };
+      # Peers of the network
+      # tags.all means 'all machines' will joined
+      roles.peer.tags.all = { };
+    };
+
+  };
+}
