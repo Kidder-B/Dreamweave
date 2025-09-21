d815746fd532e35b5d1ce4b2db6dbacd2fc19ba8
Kidder-B <kidderbrett@gmail.com>
2025-09-21 22:23:53 +0000

    Added admin.nix service module

---
+++ modules/clan/services/clan-core/admin.nix
@@ -0,0 +1,23 @@
+{
+  # Docs: See https://docs.clan.lol/reference/clanServices
+  clan.inventory.instances = {
+
+    # Docs: https://docs.clan.lol/reference/clanServices/admin/
+    # Admin service for managing machines
+    # This service adds a root password and SSH access.
+    admin = {
+      roles.default.tags.all = { };
+      roles.default.settings.allowedKeys = {
+        # Insert the public key that you want to use for SSH access.
+        # All keys will have ssh access to all machines ("tags.all" means 'all machines').
+        # Alternatively set 'users.users.root.openssh.authorizedKeys.keys' in each machine
+        "admin-machine-1" =
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+LhElRovxT0LgPhodvbh0TIsEDlRPrAF7JxvcjH0s1 brettk@nixos";
+        "admin-machine-2" =
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfOhubXnAeYNgXq+XQG87RD/rvT8p0euZdTbxB0X+gd nixbld@nixos";
+      };
+    };
+
+  };
+
+}
