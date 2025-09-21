45e794889c1fbe4510550ebde529a717c73b1702
Kidder-B <kidderbrett@gmail.com>
2025-09-21 22:09:22 +0000

    Removed tor setup from clan.nix

--- modules/clan/clan.nix
+++ modules/clan/clan.nix
@@ -37,13 +37,6 @@
         roles.peer.tags.all = { };
       };
 
-      # Docs: https://docs.clan.lol/reference/clanServices/tor/
-      # Tor network provides secure, anonymous connections to your machines
-      # All machines will be accessible via Tor as a fallback connection method
-      tor = {
-        roles.server.tags.nixos = { };
-      };
-
     };
   };
 }
