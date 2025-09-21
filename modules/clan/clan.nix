42299544ad385b78b3587e309cb359d600a11cf5
Kidder-B <kidderbrett@gmail.com>
2025-09-21 22:24:38 +0000

    Removed admin config from clan.nix

--- modules/clan/clan.nix
+++ modules/clan/clan.nix
@@ -2,26 +2,5 @@
   clan = {
     # Ensure this is unique among all clans you want to use.
     meta.name = "Dreamweave";
-
-    # Docs: See https://docs.clan.lol/reference/clanServices
-    inventory.instances = {
-
-      # Docs: https://docs.clan.lol/reference/clanServices/admin/
-      # Admin service for managing machines
-      # This service adds a root password and SSH access.
-      admin = {
-        roles.default.tags.all = { };
-        roles.default.settings.allowedKeys = {
-          # Insert the public key that you want to use for SSH access.
-          # All keys will have ssh access to all machines ("tags.all" means 'all machines').
-          # Alternatively set 'users.users.root.openssh.authorizedKeys.keys' in each machine
-          "admin-machine-1" =
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+LhElRovxT0LgPhodvbh0TIsEDlRPrAF7JxvcjH0s1 brettk@nixos";
-          "admin-machine-2" =
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfOhubXnAeYNgXq+XQG87RD/rvT8p0euZdTbxB0X+gd nixbld@nixos";
-        };
-      };
-
-    };
   };
 }
