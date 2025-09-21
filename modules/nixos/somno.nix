5ab24fcd6bdd3cdbfb0a31146a1341199d7e039f
Kidder-B <kidderbrett@gmail.com>
2025-09-21 05:01:40 +0000

    Created Somno module

---
+++ modules/nixos/somno.nix
@@ -0,0 +1,31 @@
+{
+  flake.modules.nixos."Somno" = {
+    boot.initrd.systemd = {
+      enable = true;
+    };
+
+    users.root.shell = "/bin/systemd-tty-ask-password-agent";
+
+    boot.initrd.network = {
+      enable = true;
+
+      ssh = {
+        enable = true;
+        port = 7172;
+        authorizedKeys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfOhubXnAeYNgXq+XQG87RD/rvT8p0euZdTbxB0X+gd nixbld@nixos"
+        ];
+        hostKeys = [
+          "/var/lib/initrd_host_ed25519_key"
+          "/var/lib/initrd_host_rsa_key"
+        ];
+      };
+    };
+    boot.initrd.availableKernelModules = [
+      "xhci_pci"
+    ];
+
+    # Find out the required network card driver by running `lspci -k` on the target machine
+    boot.initrd.kernelModules = [ "r8169" ];
+  };
+}
