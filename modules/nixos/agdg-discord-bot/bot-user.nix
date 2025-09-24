d15c9b1a7e3ccca27ddf41382825edccdf9542f4
Kidder-B <kidderbrett@gmail.com>
2025-09-24 19:25:21 +0000

    Adjusted user declaration

--- modules/nixos/agdg-discord-bot/bot-user.nix
+++ modules/nixos/agdg-discord-bot/bot-user.nix
@@ -1,27 +1,18 @@
 {
-  flake.modules.nixos."agdgDiscordBot" =
-    {
-      ...
-    }:
-    {
-      users = {
-        groups = {
-          containers = { };
-          redbot = { };
-        };
-
-        users.containers = {
-          group = "containers";
-          isSystemUser = true;
-          autoSubUidGidRange = true;
-        };
-
-        extraUsers.redbot = {
-          isSystemUser = true;
-          group = "redbot";
-          uid = 210;
-          home = "/var/lib/redbot";
+  flake.modules.nixos."agdgDiscordBot" = {
+    users = {
+      groups = {
+        redbot = {
+          gid = 210;
         };
       };
+
+      extraUsers.redbot = {
+        isSystemUser = true;
+        group = "redbot";
+        uid = 210;
+        home = "/var/lib/redbot";
+      };
     };
+  };
 }
