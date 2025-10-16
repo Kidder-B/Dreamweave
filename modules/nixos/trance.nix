c26e09b3bb8d70f49c57b08ddf1c96f4cea7fb26
clan-tool <clan@example.com>
2025-10-15 22:53:55 -0600

    Added user Brett's keys to the list of authorized keys

--- modules/nixos/trance.nix
+++ modules/nixos/trance.nix
@@ -14,6 +14,7 @@
         enable = true;
         port = 7172;
         authorizedKeys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOG1j99Pbv8XbbpbkJVMThRo05gFeR/26W+0vXMUecN nixbld@Trance"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfOhubXnAeYNgXq+XQG87RD/rvT8p0euZdTbxB0X+gd nixbld@nixos"
         ];
         hostKeys = [
