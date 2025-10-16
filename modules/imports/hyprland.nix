4d28cc39ae63f512cefb80e686f67b1abc4270a5
Kidder-B <kidderbrett@gmail.com>
2025-10-16 03:28:25 +0000

    Renamed plugins file to be more reflective of its function, and changed to developement branch of Hyprland

--- modules/imports/hyprland.nix
+++ modules/imports/hyprland.nix
@@ -1,6 +1,6 @@
 {
   flake-file.inputs = {
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "github:hyprwm/Hyprland?submodules=1";
     hy3 = {
       url = "github:outfoxxed/hy3";
       inputs.hyprland.follows = "hyprland";
