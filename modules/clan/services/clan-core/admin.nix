c6c348259309efe4aa9c905d91291153fdb1ce90
clan-tool <clan@example.com>
2025-10-15 23:22:34 -0600

    Set admin-machine-key to be user Brett

--- modules/clan/services/clan-core/admin.nix
+++ modules/clan/services/clan-core/admin.nix
@@ -12,10 +12,6 @@
         # All keys will have ssh access to all machines ("tags.all" means 'all machines').
         # Alternatively set 'users.users.root.openssh.authorizedKeys.keys' in each machine
         "admin-machine-1" =
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+LhElRovxT0LgPhodvbh0TIsEDlRPrAF7JxvcjH0s1 brettk@nixos";
-        "admin-machine-2" =
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEfOhubXnAeYNgXq+XQG87RD/rvT8p0euZdTbxB0X+gd nixbld@nixos";
-        "admin-machine-3" =
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOG1j99Pbv8XbbpbkJVMThRo05gFeR/26W+0vXMUecN nixbld@Trance";
       };
     };
