e80e9fb0aa82eba4ae4acbfb3414cfda73db75ff
Kidder-B <kidderbrett@gmail.com>
2025-10-17 00:15:15 +0000

    Updated Hyprland import file

--- modules/imports/hyprland.nix
+++ modules/imports/hyprland.nix
@@ -1,7 +1,34 @@
 {
   flake-file.inputs = {
-    hyprland.url = "github:hyprwm/Hyprland?submodules=1";
-    hy3 = {
+
+    hyprutils = {
+      url = "github:hyprwm/hyprutils";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
+
+    hyprlang = {
+      url = "github:hyprwm/hyprlang";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+        hyprutils.follows = "hyprutils";
+      };
+    };
+
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+        hyprutils.follows = "hyprutils";
+        hyprlang.follows = "hyprlang";
+      };
+    };
+
+    hyprland-hy3 = {
       url = "github:outfoxxed/hy3";
       inputs.hyprland.follows = "hyprland";
     };
@@ -9,6 +36,7 @@
       url = "github:VirtCode/hypr-dynamic-cursors";
       inputs.hyprland.follows = "hyprland";
     };
+
     quickshell = {
       url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
