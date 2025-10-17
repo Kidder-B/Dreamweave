c5fb731ee23b8ca48d842cdc031f927c5e122656
Kidder-B <kidderbrett@gmail.com>
2025-10-17 21:48:14 +0000

    Using Cachix

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -1,12 +1,15 @@
 {
-  flake.modules.nixos.hyprland =
-    { ... }:
-    {
-      programs.hyprland = {
-        # Install the packages from nixpkgs
-        enable = true;
-        # Whether to enable XWayland
-        xwayland.enable = true;
-      };
+  flake.modules.nixos.hyprland = {
+    nix.settings = {
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
     };
+    programs.hyprland = {
+      # Install the packages from nixpkgs
+      enable = true;
+      # Whether to enable XWayland
+      xwayland.enable = true;
+    };
+  };
 }
