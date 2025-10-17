15156419ba9ce926cffe3f4178c9b44329f2c636
Kidder-B <kidderbrett@gmail.com>
2025-10-17 02:38:12 +0000

    Removed hyprcursor config

--- modules/home-profiles/brett/hyprland/hypr-dynamic-cursors.nix
+++ modules/home-profiles/brett/hyprland/hypr-dynamic-cursors.nix
@@ -121,31 +121,6 @@
                   # see the `ipc` section below
                   ipc = false
               }
-
-              # use hyprcursor to get a higher resolution texture when the cursor is magnified
-              # see the `hyprcursor` section below
-              hyprcursor {
-
-                  # use nearest-neighbour (pixelated) scaling when magnifing beyond texture size
-                  # this will also have effect without hyprcursor support being enabled
-                  # 0 / false - never use pixelated scaling
-                  # 1 / true  - use pixelated when no highres image
-                  # 2         - always use pixleated scaling
-                  nearest = true
-
-                  # enable dedicated hyprcursor support
-                  enabled = true
-
-                  # resolution in pixels to load the magnified shapes at
-                  # be warned that loading a very high-resolution image will take a long time and might impact memory consumption
-                  # -1 means we use [normal cursor size] * [shake:base option]
-                  resolution = -1
-
-                  # shape to use when clientside cursors are being magnified
-                  # see the shape-name property of shape rules for possible names
-                  # specifying clientside will use the actual shape, but will be pixelated
-                  fallback = clientside
-              }
           }
         '';
       };
