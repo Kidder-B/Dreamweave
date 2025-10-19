0fc1ce14e99babc02ccf2d2340955d5e29f20437
Kidder-B <kidderbrett@gmail.com>
2025-10-19 22:46:53 +0000

    Formatting

--- machines/Ebullient/disko.nix
+++ machines/Ebullient/disko.nix
@@ -1,94 +1,94 @@
-  {
-    lib,
-    ...
-  }:
-  let
-    mkPool = idx: {
-      # suffix is to prevent disk name collisions
-      name = idx;
-      type = "disk";
-      device = "/dev/disk/by-id/${idx}";
-      content = {
-        type = "gpt";
-        partitions = {
-          "boot" = lib.mkIf (idx == "nvme-WD_BLACK_SN770_1TB_242810801500") {
-            size = "1M";
-            type = "EF02"; # for grub MBR
-            priority = 1;
+{
+  lib,
+  ...
+}:
+let
+  mkPool = idx: {
+    # suffix is to prevent disk name collisions
+    name = idx;
+    type = "disk";
+    device = "/dev/disk/by-id/${idx}";
+    content = {
+      type = "gpt";
+      partitions = {
+        "boot" = lib.mkIf (idx == "nvme-WD_BLACK_SN770_1TB_242810801500") {
+          size = "1M";
+          type = "EF02"; # for grub MBR
+          priority = 1;
+        };
+        "ESP" = lib.mkIf (idx == "nvme-WD_BLACK_SN770_1TB_242810801500") {
+          size = "1G";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "nofail" ];
           };
-          "ESP" = lib.mkIf (idx == "nvme-WD_BLACK_SN770_1TB_242810801500") {
-            size = "1G";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "nofail" ];
-            };
-          };
-          "root" = {
-            size = "100%";
-            content = {
-              type = "zfs";
-              pool = "zroot";
-            };
+        };
+        "root" = {
+          size = "100%";
+          content = {
+            type = "zfs";
+            pool = "zroot";
           };
         };
       };
     };
-  in
-  {
-    imports = [ ];
+  };
+in
+{
+  imports = [ ];
 
-    config = {
-      boot.loader.systemd-boot.enable = true;
+  config = {
+    boot.loader.systemd-boot.enable = true;
 
-      disko.devices = {
-        disk = {
-          x = mkPool "nvme-WD_BLACK_SN770_1TB_242810801500";
-          y = mkPool "nvme-WD_BLACK_SN770M_500GB_243868400083";
-        };
-        zpool = {
-          zroot = {
-            type = "zpool";
-            rootFsOptions = {
-              compression = "lz4";
-              acltype = "posixacl";
-              xattr = "sa";
-              "com.sun:auto-snapshot" = "true";
-              mountpoint = "none";
+    disko.devices = {
+      disk = {
+        x = mkPool "nvme-WD_BLACK_SN770_1TB_242810801500";
+        y = mkPool "nvme-WD_BLACK_SN770M_500GB_243868400083";
+      };
+      zpool = {
+        zroot = {
+          type = "zpool";
+          rootFsOptions = {
+            compression = "lz4";
+            acltype = "posixacl";
+            xattr = "sa";
+            "com.sun:auto-snapshot" = "true";
+            mountpoint = "none";
+          };
+          datasets = {
+            "root" = {
+              type = "zfs_fs";
+              options = {
+                mountpoint = "none";
+                encryption = "aes-256-gcm";
+                keyformat = "passphrase";
+                keylocation = "file:///tmp/secret.key";
+              };
             };
-            datasets = {
-              "root" = {
-                type = "zfs_fs";
-                options = {
-                  mountpoint = "none";
-                  encryption = "aes-256-gcm";
-                  keyformat = "passphrase";
-                  keylocation = "file:///tmp/secret.key";
-                };
-              };
-              "root/nixos" = {
-                type = "zfs_fs";
-                options.mountpoint = "/";
-                mountpoint = "/";
-              };
-              "root/home" = {
-                type = "zfs_fs";
-                options.mountpoint = "/home";
-                mountpoint = "/home";
-              };
-              "root/tmp" = {
-                type = "zfs_fs";
+            "root/nixos" = {
+              type = "zfs_fs";
+              options.mountpoint = "/";
+              mountpoint = "/";
+            };
+            "root/home" = {
+              type = "zfs_fs";
+              options.mountpoint = "/home";
+              mountpoint = "/home";
+            };
+            "root/tmp" = {
+              type = "zfs_fs";
+              mountpoint = "/tmp";
+              options = {
                 mountpoint = "/tmp";
-                options = {
-                  mountpoint = "/tmp";
-                  sync = "disabled";
-                };
+                sync = "disabled";
               };
             };
           };
         };
       };
     };
-  }
+  };
+}
