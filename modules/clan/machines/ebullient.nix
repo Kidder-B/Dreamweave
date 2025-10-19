0a719fad28fbbf9a86266e31f9c3bce02b9def9d
Kidder-B <kidderbrett@gmail.com>
2025-10-19 16:54:18 +0000

    Created machine Ebullient

---
+++ modules/clan/machines/ebullient.nix
@@ -0,0 +1,17 @@
+{
+  clan.inventory.machines = {
+    "Ebullient" = {
+      tags = [
+        "physical-machine"
+        "ebullient"
+        "brett"
+        "nvidia"
+        "unfree"
+        "obs-virtual-camera-support"
+        "hyprland"
+        "gaming"
+        "client"
+      ];
+    };
+  };
+}
