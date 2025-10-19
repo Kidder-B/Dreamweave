ff8818799e9a4fd5805c48156b8bb46fc0e06191
Kidder-B <kidderbrett@gmail.com>
2025-10-19 17:40:28 +0000

    Reduced Ebullient tags for initial install

--- modules/clan/machines/ebullient.nix
+++ modules/clan/machines/ebullient.nix
@@ -4,13 +4,6 @@
       tags = [
         "physical-machine"
         "ebullient"
-        "brett"
-        "nvidia"
-        "unfree"
-        "obs-virtual-camera-support"
-        "hyprland"
-        "gaming"
-        "client"
       ];
     };
   };
