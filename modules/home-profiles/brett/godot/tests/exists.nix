2e64bbb7815ad6b39a8c10867ec2d14fffaacfb8
Brett Kidder <30retton@gmail.com>
2025-11-03 20:51:13 +0000

    test: implemented minimal test from nixos docs

--- modules/home-profiles/brett/godot/tests/exists.nix
+++ modules/home-profiles/brett/godot/tests/exists.nix
@@ -1,5 +1,28 @@
 {
-  perSystem.checks = {
+  perSystem =
+    {
+      pkgs,
+      ...
+    }:
+    {
+      checks.default = {
+        name = "minimal-test";
+        nodes.machine = {
+          users.users.alice = {
+            isNormalUser = true;
+            extraGroups = [ "wheel" ];
+            packages = with pkgs; [
+              firefox
+              tree
+            ];
+          };
+        };
 
-  };
+        testScript = ''
+          machine.wait_for_unit("default.target")
+          machine.succeed("su -- alice -c 'which firefox'")
+          machine.fail("su -- root -c 'which firefox'")
+        '';
+      };
+    };
 }
