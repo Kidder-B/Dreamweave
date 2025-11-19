63cfda99a6e904716b96d30595593cb16d198e05
Brett Kidder <30retton@gmail.com>
2025-11-19 14:28:26 -0700

    Update: flake

--- flake.nix
+++ flake.nix
@@ -5,68 +5,26 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
-    clan-core = {
-      url = "https://git.clan.lol/clan/clan-core/archive/main.tar.gz";
-    };
-    den = {
-      url = "github:vic/den";
-    };
-    devshell = {
-      url = "github:numtide/devshell";
-    };
-    flake-aspects = {
-      url = "github:vic/flake-aspects";
-    };
-    flake-file = {
-      url = "github:vic/flake-file";
-    };
+    clan-core.url = "https://git.clan.lol/clan/clan-core/archive/main.tar.gz";
+    den.url = "github:vic/den";
+    devshell.url = "github:numtide/devshell";
+    flake-aspects.url = "github:vic/flake-aspects";
+    flake-file.url = "github:vic/flake-file";
     flake-parts = {
       follows = "clan-core/flake-parts";
-      inputs = {
-        nixpkgs-lib = {
-          follows = "nixpkgs-lib";
-        };
-      };
+      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
       url = "github:hercules-ci/flake-parts";
     };
-    home-manager = {
-      url = "github:nix-community/home-manager";
-    };
-    import-tree = {
-      url = "github:vic/import-tree";
-    };
-    khanelivim = {
-      url = "github:khaneliman/khanelivim";
-    };
-    nix-auto-follow = {
-      inputs = {
-        nixpkgs = {
-          follows = "nixpkgs";
-        };
-      };
-      url = "github:fzakaria/nix-auto-follow";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    import-tree.url = "github:vic/import-tree";
+    khanelivim.url = "github:khaneliman/khanelivim";
     nixpkgs = {
       follows = "clan-core/nixpkgs";
       url = "github:nixos/nixpkgs/nixpkgs-unstable";
     };
-    nixpkgs-lib = {
-      follows = "nixpkgs";
-    };
-    quadlet-nix = {
-      url = "github:SEIAROTg/quadlet-nix";
-    };
-    systems = {
-      url = "github:nix-systems/default";
-    };
-    treefmt-nix = {
-      inputs = {
-        nixpkgs = {
-          follows = "nixpkgs";
-        };
-      };
-      url = "github:numtide/treefmt-nix";
-    };
+    nixpkgs-lib.follows = "nixpkgs";
+    quadlet-nix.url = "github:SEIAROTg/quadlet-nix";
+    systems.url = "github:nix-systems/default";
   };
 
 }
