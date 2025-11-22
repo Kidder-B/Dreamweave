ac9c817908c19696fe691b69f17b0caab0099bf4
Brett Kidder <30retton@gmail.com>
2025-11-22 13:11:11 -0700

    Fix: Hyprland polkit nonsense

--- modules/home-profiles/brett/hyprland/hyprland.nix
+++ modules/home-profiles/brett/hyprland/hyprland.nix
@@ -1,18 +1,10 @@
 {
-  flake.modules.homeManager."Brett" =
-    { lib, pkgs, ... }:
-    {
-      wayland.windowManager.hyprland = {
-        enable = true;
-        package = null;
-        portalPackage = null;
-        extraConfig = ''
-          exec-once = waybar
-        '';
-      };
-      xdg.portal = {
-        enable = lib.mkForce true;
-        extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
-      };
+  flake.modules.homeManager."Brett" = {
+    wayland.windowManager.hyprland = {
+      enable = true;
+      extraConfig = ''
+        exec-once = waybar
+      '';
     };
+  };
 }
