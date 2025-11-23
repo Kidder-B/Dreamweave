d6ae4d04b1b793a4d44727f711bd4d1ee2f3781b
Brett Kidder <30retton@gmail.com>
2025-11-23 14:25:31 -0700

    Add: Service Syncthing

---
+++ modules/clan/services/clan-core/syncthing.nix
@@ -0,0 +1,15 @@
+{
+  clan.inventory.instances = {
+    syncthing = {
+      roles.peer.tags.all = { };
+      roles.peer.settings.folders = {
+        Blender = {
+          path = "/home/Brett/Blender/";
+        };
+        Godot = {
+          path = "/home/Brett/Godot/";
+        };
+      };
+    };
+  };
+}
