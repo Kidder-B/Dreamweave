2b9d89de67a645359a98a9de83614956833e2647
Brett Kidder <30retton@gmail.com>
2026-01-06 15:38:36 -0700

    Fix: emacs

--- modules/emacs/system.nix
+++ modules/emacs/system.nix
@@ -2,13 +2,8 @@
   flake.modules.nixos.emacs =
     { pkgs, ... }:
     {
-      environment.systemPackages = [
-        pkgs.emacs
-      ];
-
-      services.emacs.enable = true;
-      
-      programs.emacs = {
+      services.emacs = {
+        package = pkgs.emacs;
         enable = true;
         defaultEditor = true;
       };
