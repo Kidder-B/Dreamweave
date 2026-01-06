0f121c1d4ac27d4d540656fe267961e396eff860
Brett Kidder <30retton@gmail.com>
2026-01-06 15:30:55 -0700

    Update: emacs to stable version

--- modules/emacs/system.nix
+++ modules/emacs/system.nix
@@ -4,15 +4,9 @@
     {
       services.emacs.enable = true;
       
-      nixpkgs.overlays = [
-        (import (builtins.fetchTarball {
-	  url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-	  sha256 = "7b7d24b82fcc577e64808cc5dd60a51b4403b4d74e6d0dab833ab8a9effc6b92";
-	}))
-      ];
-
-      environment.systemPackages = [
-        pkgs.emacsGcc  # Installs Emacs 28 + native-comp
-      ];
+      programs.emacs = {
+        enable = true;
+        defaultEditor = true;
+      };
     };
 }
