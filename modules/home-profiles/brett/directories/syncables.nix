574ec80baf9dc3a5ef03c6941e8c3e90ddf1447d
Brett Kidder <30retton@gmail.com>
2026-01-22 17:10:29 -0700

    Fix: Brett being added to dir id

--- modules/home-profiles/brett/directories/syncables.nix
+++ modules/home-profiles/brett/directories/syncables.nix
@@ -4,7 +4,7 @@
     let
       createSyncableDirs = dirs: lib.hm.dag.entryAfter ["writeBoundary"] ''
         for dir in ${toString (lib.attrNames dirs)}; do
-          mkdir -p $VERBOSE_ARG "$HOME/Brett/$dir"
+          mkdir -p $VERBOSE_ARG "$HOME/$dir"
           chgrp syncthing $VERBOSE_ARG "$HOME/Brett/$dir" || echo "Failed to set group for $dir directory."
         done
       '';
