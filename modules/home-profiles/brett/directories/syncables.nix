1595b89e5e7f5d015ced87f8e1d935a552c4072e
Kidder-B <kidderbrett@gmail.com>
2026-01-22 17:00:41 -0700

    Add: Syncables.nix for user Brett

---
+++ modules/home-profiles/brett/directories/syncables.nix
@@ -0,0 +1,23 @@
+{ lib, ... }:
+
+let
+  createSyncableDirs = dirs: lib.hm.dag.entryAfter ["writeBoundary"] ''
+    for dir in ${lib.toString (lib.attrNames dirs)}; do
+      mkdir -p $VERBOSE_ARG "$HOME/Brett/$dir"
+      chgrp syncthing $VERBOSE_ARG "$HOME/Brett/$dir" || echo "Failed to set group for $dir directory."
+    done
+  '';
+in
+{
+  flake.modules.homeManager."Brett" =
+    {
+      home.activation = {
+        syncableDirs = createSyncableDirs {
+          # Define Syncables here
+          Blender = "Blender/Syncable";
+          Clan = "Clan/Dreamweave";
+          Godot = "Godot/Syncable";
+        };
+      };
+    };
+}
