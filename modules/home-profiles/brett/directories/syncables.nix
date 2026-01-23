6dda868ab029ea09318818e88b3f918801cd4e1a
Brett Kidder <30retton@gmail.com>
2026-01-22 17:07:15 -0700

    Fix: Typo

--- modules/home-profiles/brett/directories/syncables.nix
+++ modules/home-profiles/brett/directories/syncables.nix
@@ -3,7 +3,7 @@
     { lib, ... }:
     let
       createSyncableDirs = dirs: lib.hm.dag.entryAfter ["writeBoundary"] ''
-        for dir in ${lib.toString (lib.attrNames dirs)}; do
+        for dir in ${toString (lib.attrNames dirs)}; do
           mkdir -p $VERBOSE_ARG "$HOME/Brett/$dir"
           chgrp syncthing $VERBOSE_ARG "$HOME/Brett/$dir" || echo "Failed to set group for $dir directory."
         done
