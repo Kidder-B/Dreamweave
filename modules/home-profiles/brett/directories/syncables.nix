6742747befb6678a03f95604b5e7f145bc99fc37
Brett Kidder <30retton@gmail.com>
2026-01-22 19:52:26 -0700

    Fix: tweak to ignore files user cannot change

--- modules/home-profiles/brett/directories/syncables.nix
+++ modules/home-profiles/brett/directories/syncables.nix
@@ -8,14 +8,16 @@
           for dir in ${lib.concatStringsSep " " dirs}; do
             mkdir -p $VERBOSE_ARG "$HOME/$dir"
 
-            chgrp -R syncthing $VERBOSE_ARG "$HOME/$dir" \
-              || echo "Failed to set group for $dir directory."
+            chgrp -R syncthing "$HOME/$dir" 2>/dev/null || true
 
-            find "$HOME/$dir" -type d -exec chmod u+rwx,g+rwx,g+s {} +
-            find "$HOME/$dir" -type f -exec chmod u+rw,g+rw {} +
+            find "$HOME/$dir" -type d -exec chmod u+rwx,g+rwx,g+s {} + 2>/dev/null || true
+            find "$HOME/$dir" -type f -exec chmod u+rw,g+rw {} + 2>/dev/null || true
 
-            chmod g+rwx $VERBOSE_ARG "$HOME/$dir"
-            chmod g+s $VERBOSE_ARG "$HOME/$dir"
+            chmod g+rwx "$HOME/$dir" 2>/dev/null || true
+            chmod g+s "$HOME/$dir" 2>/dev/null || true
+
+            setfacl -R -m g:syncthing:rwX "$HOME/$dir" 2>/dev/null || true
+            setfacl -d -m g:syncthing:rwX "$HOME/$dir" 2>/dev/null || true
           done
         '';
     in
