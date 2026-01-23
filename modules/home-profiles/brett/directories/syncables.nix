deea723e4e18967404c60fd49f1e2acc6115c884
Brett Kidder <30retton@gmail.com>
2026-01-23 13:44:45 -0700

    Fix: Group mask

--- modules/home-profiles/brett/directories/syncables.nix
+++ modules/home-profiles/brett/directories/syncables.nix
@@ -13,13 +13,13 @@
         lib.hm.dag.entryAfter ["writeBoundary"] ''
           for dir in ${lib.concatStringsSep " " dirs}; do
             mkdir -p $VERBOSE_ARG "$HOME/$dir"
+
             chgrp -R syncthing "$HOME/$dir" 2>/dev/null || true
-            find "$HOME/$dir" -type d -exec chmod u+rwx,g+rwx,g+s {} + 2>/dev/null || true
-            find "$HOME/$dir" -type f -exec chmod u+rw,g+rw {} + 2>/dev/null || true
-            chmod g+rwx "$HOME/$dir" 2>/dev/null || true
-            chmod g+s "$HOME/$dir" 2>/dev/null || true
-            setfacl -R -m g:syncthing:rwX "$HOME/$dir" 2>/dev/null || true
-            setfacl -d -m g:syncthing:rwX "$HOME/$dir" 2>/dev/null || true
+
+            find "$HOME/$dir" -type d -exec chmod g+s {} + 2>/dev/null || true
+
+            setfacl -R -m m:rwx,g:syncthing:rwX "$HOME/$dir" 2>/dev/null || true
+            setfacl -d -m m:rwx,g:syncthing:rwX "$HOME/$dir" 2>/dev/null || true
           done
         '';
     in
