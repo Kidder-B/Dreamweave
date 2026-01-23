0f9d01825966973f4509dfb344c5af84d7f6f4c2
Brett Kidder <30retton@gmail.com>
2026-01-22 17:13:29 -0700

    Fix: Incorrect path

--- modules/home-profiles/brett/directories/syncables.nix
+++ modules/home-profiles/brett/directories/syncables.nix
@@ -5,7 +5,7 @@
       createSyncableDirs = dirs: lib.hm.dag.entryAfter ["writeBoundary"] ''
         for dir in ${toString (lib.attrNames dirs)}; do
           mkdir -p $VERBOSE_ARG "$HOME/$dir"
-          chgrp syncthing $VERBOSE_ARG "$HOME/Brett/$dir" || echo "Failed to set group for $dir directory."
+          chgrp syncthing $VERBOSE_ARG "$HOME/$dir" || echo "Failed to set group for $dir directory."
         done
       '';
     in
