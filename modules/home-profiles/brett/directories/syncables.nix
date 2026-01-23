e252925e6d605c7a6d91cabce1d3c8e01492e4bf
Brett Kidder <30retton@gmail.com>
2026-01-22 19:47:46 -0700

    Modify: Permission behavior of syncable directory

--- modules/home-profiles/brett/directories/syncables.nix
+++ modules/home-profiles/brett/directories/syncables.nix
@@ -7,8 +7,13 @@
         lib.hm.dag.entryAfter ["writeBoundary"] ''
           for dir in ${lib.concatStringsSep " " dirs}; do
             mkdir -p $VERBOSE_ARG "$HOME/$dir"
-            chgrp syncthing $VERBOSE_ARG "$HOME/$dir" \
+
+            chgrp -R syncthing $VERBOSE_ARG "$HOME/$dir" \
               || echo "Failed to set group for $dir directory."
+
+            find "$HOME/$dir" -type d -exec chmod u+rwx,g+rwx,g+s {} +
+            find "$HOME/$dir" -type f -exec chmod u+rw,g+rw {} +
+
             chmod g+rwx $VERBOSE_ARG "$HOME/$dir"
             chmod g+s $VERBOSE_ARG "$HOME/$dir"
           done
