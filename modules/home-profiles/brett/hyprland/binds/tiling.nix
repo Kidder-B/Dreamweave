ae55a3086ef9f4dd29367f6572c1a71fda72c225
Brett Kidder <30retton@gmail.com>
2026-03-02 16:56:58 -0700

    Config: Hyprland keybinds

---
+++ modules/home-profiles/brett/hyprland/binds/tiling.nix
@@ -0,0 +1,25 @@
+{
+  flake.modules.homeManager."Brett" = {
+    wayland.windowManager.hyprland.settings = {
+      bind =
+        [
+          # Toggle floating windows
+          "$mod, V, togglefloating"
+
+          # Move focus with arrow keys
+          "$mod, left, movefocus, l"
+          "$mod, right, movefocus, r"
+          "$mod, up, movefocus, u"
+          "$mod, down, movefocus, d"
+
+          # Scroll through existing workspaces
+          "$mod, mouse_down, workspace, e+1"
+          "$mod, mouse_up, workspace, e-1"
+
+          # Move/resize windows with mainMod + LMB/RMB and dragging
+          "$mod, mouse:272, movewindow"  # Left mouse button
+          "$mod, mouse:273, resizewindow" # Right mouse button
+        ]
+    };
+  };
+}
